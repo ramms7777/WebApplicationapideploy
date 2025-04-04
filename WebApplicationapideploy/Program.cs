b550a2cb744d1c9e455dea2aae9704c4ef2dac90
Dhanush Ram Suresh <dhanush@qonsult.systems>
2025-04-04 09:46:20 +0530

    changes

--- WebApplicationapideploy/Program.cs
+++ WebApplicationapideploy/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.WebHost.UseUrls("http://*:8080"); // Change to desired port
+builder.WebHost.UseUrls("http://*:5000"); // Change to desired port
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
