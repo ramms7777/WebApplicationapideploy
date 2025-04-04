c95e74d99deecd44e9b5e0787356e1c8b17ab5ac
Dhanush Ram Suresh <dhanush@qonsult.systems>
2025-04-04 09:22:19 +0530

    changes in code

--- WebApplicationapideploy/Program.cs
+++ WebApplicationapideploy/Program.cs
@@ -1,25 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Register controller services
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.MapGet("/", () => "Hello, World!"); // Test route
 
-app.UseHttpsRedirection();
+app.UseRouting();
 
-app.UseAuthorization();
-
-app.MapControllers();
+// Ensure controller endpoints are available
+app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
 
 app.Run();
