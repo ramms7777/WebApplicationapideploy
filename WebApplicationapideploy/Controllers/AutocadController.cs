7bb337987bf3e9848afb754c7a92763212438351
Dhanush Ram Suresh <dhanush@qonsult.systems>
2025-04-25 10:28:25 +0530

    changes

--- WebApplicationapideploy/Controllers/AutocadController.cs
+++ WebApplicationapideploy/Controllers/AutocadController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace WebApplicationapideploy.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("autocad")]
 public class AutoCADController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
